37199db2b57ff0e96b449dd42c8fdb270cdd763a
filip <fbielejec@gmail.com>
2019-09-04 08:51:28 +0200

    move all config

--- customizations/setup-rust.el
+++ customizations/setup-rust.el
@@ -1,5 +1,9 @@
 (add-to-list 'auto-mode-alist '("\\.rs$" . rust-mode))
 
+(defun rust-save-hook()
+  (when (eq major-mode 'rust-mode)
+    (whitespace-cleanup)))
+
 (use-package racer
   :requires rust-mode
 
@@ -11,14 +15,8 @@
   :config
   (add-hook 'rust-mode-hook #'racer-mode)
   (add-hook 'racer-mode-hook #'eldoc-mode)
-  (add-hook 'racer-mode-hook #'company-mode))
-
-(add-hook 'rust-mode-hook 'enable-paredit-mode)
-(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
-(setq company-tooltip-align-annotations t)
-
-(defun rust-save-hook()
-  (when (eq major-mode 'rust-mode)
-    (whitespace-cleanup)))
-
-(add-hook 'before-save-hook 'rust-save-hook)
+  (add-hook 'racer-mode-hook #'company-mode)
+  (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+  (add-hook 'rust-mode-hook 'enable-paredit-mode)
+  (setq company-tooltip-align-annotations t)
+  (add-hook 'before-save-hook 'rust-save-hook))
