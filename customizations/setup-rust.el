b62a4ad3a8fc3a73d93496870bc9bf03740a4b5f
filip <fbielejec@gmail.com>
2019-10-17 12:08:20 +0200

    fix rust setup broken by use-package (or my poor understanding if it)

--- customizations/setup-rust.el
+++ customizations/setup-rust.el
@@ -1,23 +1,39 @@
-(require 'use-package)
-(add-to-list 'auto-mode-alist '("\\.rs$" . rust-mode))
+;; (require 'use-package)
 
 (defun rust-save-hook()
   (when (eq major-mode 'rust-mode)
     (whitespace-cleanup)))
 
-(use-package racer
-  :requires rust-mode
+;; (use-package racer
+;;   :requires rust-mode
 
-  :init (setq racer-rust-src-path
-              (concat (string-trim
-                       (shell-command-to-string "rustc --print sysroot"))
-                      "/lib/rustlib/src/rust/src"))
+;;   :init (setq racer-rust-src-path
+;;               (concat (string-trim
+;;                        (shell-command-to-string "rustc --print sysroot"))
+;;                       "/lib/rustlib/src/rust/src"))
 
-  :config
-  (add-hook 'rust-mode-hook 'enable-paredit-mode)
-  (add-hook 'before-save-hook 'rust-save-hook)
-  (add-hook 'rust-mode-hook #'racer-mode)
-  (add-hook 'racer-mode-hook #'company-mode)
-  (add-hook 'racer-mode-hook #'eldoc-mode)
-  (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
-  (setq company-tooltip-align-annotations t))
+;;   :config
+;;   (add-hook 'rust-mode-hook 'enable-paredit-mode)
+;;   (add-hook 'before-save-hook 'rust-save-hook)
+;;   (add-hook 'rust-mode-hook #'racer-mode)
+;;   (add-hook 'racer-mode-hook #'company-mode)
+;;   (add-hook 'racer-mode-hook #'eldoc-mode)
+;;   (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+;;   (setq company-tooltip-align-annotations t))
+
+(require 'rust-mode)
+
+(add-to-list 'auto-mode-alist '("\\.rs$" . rust-mode))
+
+(setq racer-rust-src-path
+      (concat (string-trim
+               (shell-command-to-string "rustc --print sysroot"))
+              "/lib/rustlib/src/rust/src"))
+
+(add-hook 'rust-mode-hook 'enable-paredit-mode)
+(add-hook 'before-save-hook 'rust-save-hook)
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'company-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+(define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
+(setq company-tooltip-align-annotations t)
