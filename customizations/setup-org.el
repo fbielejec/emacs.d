f093bd54d90ac23825f4006ec19ea56da337e934
filip <fbielejec@gmail.com>
2025-05-19 12:50:03 +0200

    org mode dot

--- customizations/setup-org.el
+++ customizations/setup-org.el
@@ -4,6 +4,7 @@
 
 ;; Start-up with line wrapping (toggle-truncate-lines)
 (setq org-startup-truncated nil)
+
 ;; override org mode keybindings
 (eval-after-load "org"
   '(progn
@@ -12,18 +13,11 @@
      (define-key org-mode-map (kbd "<M-left>") nil)
      (define-key org-mode-map (kbd "<M-right>") nil)
      ))
-;; orgmode gets slow
-;;(setq undo-limit 50)
 
-;; configure org-mode for Clojure code blocks
+;; orgmode gets slow
+;; (setq undo-limit 50)
+
 (require 'ob)
-;; (require 'ob-clojure)
-;; ;;(require 'org-babel-clojure)
-;; (setq org-babel-clojure-backend 'cider)
-;; (require 'cider)
-;; (org-babel-do-load-languages
-;;  'org-babel-load-languages
-;;  '((clojure . t)))
 
  ;; Do not confirm before evaluation
 (setq org-confirm-babel-evaluate nil)
@@ -34,8 +28,7 @@
 
 ;; ;; org files to build the agenda from
 (setq org-agenda-files '("~/CloudStation/"
-                         "~/CloudStation/scrapbook.org"
-                         "~/CloudStation/aleph/"))
+                         "~/CloudStation/scrapbook.org"))
 
 ;; custom agenda view
 (defun air-org-skip-subtree-if-priority (priority)
@@ -59,12 +52,6 @@
                      '(or (air-org-skip-subtree-if-priority ?A)
                           (org-agenda-skip-if nil '(scheduled deadline))))))))))
 
-;; integrate plantuml [diagrams] support
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '(;; other Babel languages
-   (plantuml . t)))
-
 ;; <dependency>
 ;;     <groupId>net.sourceforge.plantuml</groupId>
 ;;     <artifactId>plantuml</artifactId>
@@ -74,11 +61,6 @@
 
 (setq org-plantuml-jar-path
       (expand-file-name "/home/filip/Programs/plantuml-nodot.1.2023.7.jar"))
-
-;; setup Maxima Source Code Blocks
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((maxima . t))) ; this line activates maxima
 
 ;; python/jupyter/maxima code blocks
 ;; (setq ob-ipython-command "/home/filip/.local/bin/jupyter")
@@ -91,6 +73,7 @@
 
 (org-babel-do-load-languages 'org-babel-load-languages
   '((C . t)
+    (plantuml . t)
     (emacs-lisp . t)
     (fortran . t)
     (gnuplot . t)
@@ -102,26 +85,10 @@
     (org . t)
     (R . t)
     (shell . t)
+    (maxima . t)
+    ;; graphviz
+    (dot . t)
 ))
-
-;; (setq sage-shell:sage-executable "/usr/bin/sage")
-
-;; (sage-shell:define-alias)
-;; ;; Turn on eldoc-mode
-;; (add-hook 'sage-shell-mode-hook #'eldoc-mode)
-;; (add-hook 'sage-shell:sage-mode-hook #'eldoc-mode)
-
-;; (setq sage-shell:use-prompt-toolkit t)
-;; (setq sage-shell:completion-function 'pcomplete)
-
-;; (require 'ob-sagemath)
-;; ;; Ob-sagemath supports only evaluating with a session.
-;; (setq org-babel-default-header-args:sage '((:session . t)
-;;                                            (:results . "output")))
-
-;; ;; C-c s for asynchronous evaluating (only for SageMath code blocks).
-;; (with-eval-after-load "org"
-;;   (define-key org-mode-map (kbd "C-c s") 'ob-sagemath-execute-async))
 
 ;; Do not confirm before evaluation
 (setq org-confirm-babel-evaluate nil)
@@ -130,43 +97,6 @@
 (defun filip/org-mode-hook ()
   ;; needs org-make-toc package
   (add-hook 'before-save-hook 'org-make-toc-mode))
-
-;; ;; Do not evaluate code blocks when exporting.
-;; (setq org-export-babel-evaluate nil)
-
-;; ;; Show images when opening a file.
-;; (setq org-startup-with-inline-images t)
-
-;; ;; Show images after evaluating code blocks.
-;; (add-hook 'org-babel-after-execute-hook 'org-display-inline-images)
-
-;; (eval-after-load "sage-shell-mode"
-;;   '(sage-shell:define-keys sage-shell-mode-map
-;;      "C-c C-i"  'helm-sage-complete
-;;      "C-c C-h"  'helm-sage-describe-object-at-point
-;;      "M-r"      'helm-sage-command-history
-;;      "C-c o"    'helm-sage-output-history))
-
-;; (setq sage-shell:input-history-cache-file "~/.emacs.d/.sage_shell_input_history")
-
-;; (add-hook 'sage-shell-after-prompt-hook #'sage-shell-view-mode)
-
-;; TODO make shift + arrow keybinding work in orgmode
-;; (setq org-support-shift-select t
-;;       org-replace-disputed-keys t)
-;; (add-hook 'org-shiftup-final-hook 'shrink-window)
-;; (add-hook 'org-shiftleft-final-hook 'enlarge-window-horizontally)
-;; (add-hook 'org-shiftdown-final-hook 'enlarge-window)
-;; (add-hook 'org-shiftright-final-hook 'shrink-window-horizontally)
-
-;; (setq org-replace-disputed-keys t)
-;; (define-key org-mode-map (kbd "S-<down>") 'enlarge-window)
-;; (define-key org-mode-map (kbd "S-<up>") 'shrink-window)
-;; (define-key org-mode-map (kbd "S-<left>") 'enlarge-window-horizontally)
-;; (define-key org-mode-map (kbd "S-<right>") 'shrink-window-horizontally)
-
-;; modern styling
-;; (add-hook 'org-mode-hook 'org-modern-mode)
 
 ;; bigger latex previews
 (setq org-format-latex-options (plist-put org-format-latex-options :scale 2.0))
